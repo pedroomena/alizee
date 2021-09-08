35abc4f26781cc495717b4995dbdeb3a2bf26291
Pedro Vieira <31294491+pedroomena@users.noreply.github.com>
2021-09-07 22:16:05 -0300

    veremos

--- bot.py
+++ bot.py
@@ -33,17 +33,31 @@ async def on_message(message):
 
 @bot.event
 async def on_message_delete(message):
-    if message.author.id == WALLACE_ID:
+    if message.author.id == WALLACE_ID and False:  # off for now
         if message.content:
             _message = 'wallace excluiu: "{}"'.format(message.content)
         else:
             _message = '\n'.join([i.proxy_url for i in message.attachments])
         await message.channel.send(_message)
 
+    if message.author.id in [ALIZEE_ID]:
+        return None
+
+
+    att = message.attachments[0].proxy_url if message.attachments else None
+    title = f'{message.author.name} excluiu uma mensagem'
+    description = '{}{}'.format(message.content, f'media: {att}' if att else '')
+    embed = Embed(title=title, description=description, color=Color.random(), url=message.jump_url)
+    embed.set_thumbnail(url=message.author.avatar_url)
+    if message.attachments:
+        embed.set_image(url=message.attachments[0].proxy_url)
+    await message.channel.guild.get_channel(884953733983793202).send(embed=embed)
+
 
 @bot.event
 async def on_message_edit(before, after):
-    if after.author.id == [WALLACE_ID, 171033510319095809] and before.content != after.content:
+    return None
+    if after.author.id in [WALLACE_ID, 171033510319095809] and before.content != after.content:
         await after.reply(f'mensagem original: {before.content}')
 
 
